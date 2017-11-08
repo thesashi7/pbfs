6eace6bfbe57bd5f9b69cda88858ba4912545840
sashi <sashi_thapaliya@hotmail.com>
2017-11-07 22:43:56 -0800

    clean up along with removal of vertex ordering

--- verf.cpp
+++ verf.cpp
@@ -18,7 +18,7 @@ typedef std::map<int, int > order_list;
 
 void read_adjacency_list(adjacency_list&, char*);
 void read_order_list(order_list&, char*);
-std::map<int, int> breadth_first_search(adjacency_list&, int, map<int, int>& );
+std::map<int, int> breadth_first_search(adjacency_list&, int );
 void print_graph( adjacency_list );
 void print_orders( map<int, int>);
 bool verify(map<int, int>, map<int, int>);
@@ -42,12 +42,10 @@ int main(int argc, char* argv[]) {
         return -1;
     }
     read_order_list(order, orderfile);
-    print_graph(graph);
     map<int, int> vert_orders;
+    map<int, int> node_level = breadth_first_search(graph, src);
 
-    map<int, int> node_level = breadth_first_search(graph, src, vert_orders);
-
-    //print_orders(vert_orders );
+    // Verification and OUTPUT GETS PRINTED HERE
     if(verify(node_level, order))
     {
         std::cout<<"True\n";
@@ -56,39 +54,7 @@ int main(int argc, char* argv[]) {
     {
         std::cout<<"False\n";
     }
-    // OUTPUT GETS PRINTED HERE
     return 0;
-}
-
-
-bool verify_2(vector<vector<int> > level_list, vector<int> order)
-{
-    bool is_correct;
-    int level_i;
-    is_correct = true;
-    level_i = 0;
-
-    for(int i=0; i < level_list.size(); i++)
-    {
-        vector<int> level;
-        level = level_list[i];
-        int cur_size;
-        cur_size = level.size();
-        if(level_i > order.size())
-            return false;
-        while(level_i < order.size())
-        {
-            int node = order[level_i];
-            if (std::find(level.begin(), level.end(),node) == level.end())
-            {
-                return false;
-            }
-            level_i++;
-        }
-    }
-    if(level_i != order.size())
-        is_correct = false;
-    return is_correct;
 }
 
 bool verify(map<int, int> node_level_s, map<int, int> node_level_p)
@@ -201,56 +167,9 @@ void print_graph( adjacency_list g ) {
     return;
 }
 
-// perform breadth-first search
-std::vector<vector<int> > breadth_first_search_2( adjacency_list& graph, int src, map<int, int>& vert_orders) {
-    std::vector<vector<int> > level_list;
-    // initialize visited and queue
-    bool* visited = new bool[graph.size()];
-    for(int i=0; i<graph.size(); i++)
-        visited[i]=false;
-    queue<int> next_verts;
-
-    /*
-     breadth-first search
-     */
-    int order = 1;
-    next_verts.push(src);
-    visited[src-1] = true;
-    while(next_verts.size()>0)
-    {
-        vector<int> level;
-        queue<int> temp_queue;
-        while(next_verts.size()>0)
-        {
-            int cur_node = next_verts.front();
-            next_verts.pop();
-            level.push_back(cur_node);
-            vert_orders.insert(std::make_pair(cur_node, order++));
-            list<int>nb;
-            map<int, std::list<int> >::iterator it = graph.find(cur_node);
-            if(it != graph.end())
-            {
-                nb = it->second;
-                std::list<int>::iterator it;
-                for (it = nb.begin(); it != nb.end(); ++it)
-                {
-                    if(visited[(*it)-1] == false)
-                    {
-                        temp_queue.push(*it);
-                        visited[(*it)-1] = true;
-                    }
-                }
-            }
-        }
-        level_list.push_back(level);
-        next_verts = temp_queue;
-    }
-
-    return level_list;
-}
 
 // perform breadth-first search
-map<int, int> breadth_first_search( adjacency_list& graph, int src, map<int, int>& vert_orders) {
+map<int, int> breadth_first_search( adjacency_list& graph, int src) {
     map<int, int> node_level;
 
     // initialize visited and queue
@@ -275,7 +194,6 @@ map<int, int> breadth_first_search( adjacency_list& graph, int src, map<int, int
             int cur_node = next_verts.front();
             next_verts.pop();
             node_level.insert(std::make_pair(cur_node, lev));
-            vert_orders.insert(std::make_pair(cur_node, order++));
             list<int>nb;
             map<int, std::list<int> >::iterator it = graph.find(cur_node);
             if(it != graph.end())
