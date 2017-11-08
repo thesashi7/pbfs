802427c5565ffa687baf69e31d8deedb14a93ae0
sashi <sashi_thapaliya@hotmail.com>
2017-11-07 22:06:23 -0800

    fixed the seg fault issue and made other modifications

--- pbfs.cpp
+++ pbfs.cpp
@@ -27,7 +27,7 @@ uint64 getTimeMs64();
 
 
 int main(int argc, char* argv[]) {
-    
+
     if(argc != 4) {
         std::cout << "usage:  ./bfs  input_graph  src num_threads" << std::endl;
         return -1;
@@ -35,36 +35,41 @@ int main(int argc, char* argv[]) {
     char* inputfile = argv[1];
     int src = atoi( argv[2] );
     int num_threads = atoi( argv[3] );
-    
+
     omp_set_num_threads(num_threads);
-    
+
     adjacency_list graph;
-    
+
     read_adjacency_list(graph, inputfile);
     if( graph.find(src) == graph.end() ) {
         cout << "error: non-existant source" << endl;
         return -1;
     }
     map<int, int> vert_orders;
-    
+    uint64 time_start, time_stop, runtime;
+
+    time_start = getTimeMs64();
     map<int, int> node_level = parallel_breadth_first_search(graph, src, vert_orders);
-    
+    time_stop = getTimeMs64();
+    runtime = (time_stop - time_start) / 10;
+    cout << "The runtime is: " << runtime << " ms" << endl;
+
     cout << "The number of threads is: " << num_threads << endl;
     generate_order_file(node_level);
-    
+
     return 0;
 }
 
 /* read an adjacency list */
 void read_adjacency_list(adjacency_list &g, char* inputfile) {
-    
+
     std::ifstream infile;
     infile.open(inputfile);
     std::string line;
-    
+
     int v1,v2;
     std::list<int> adjacent_verts;
-    
+
     // for each line, get numbers after a space
     while( getline(infile, line) ) {
         adjacent_verts.clear();
@@ -94,24 +99,23 @@ void read_adjacency_list(adjacency_list &g, char* inputfile) {
         else
             adjacent_verts.push_back(v);
         g.insert( std::make_pair( v1, adjacent_verts ) );
-        
+
     }
-    
+
     infile.close();
-    
+
     return;
 }
 
 void print_graph( adjacency_list g ) {
-    
     adjacency_list::iterator g_itr;
     list<int> row;
     list<int>::iterator row_itr;
     for( g_itr = g.begin(); g_itr != g.end(); ++g_itr ) {
-        
+
         int v = (*g_itr).first;
         cout << v << ": ";
-        
+
         row = (*g_itr).second;
         for( row_itr = row.begin(); row_itr != row.end(); ++row_itr ) {
             cout << *row_itr << " ";
@@ -119,27 +123,28 @@ void print_graph( adjacency_list g ) {
         cout << endl;
     }
     cout << endl;
-    
+
     return;
 }
 
 // perform parallel breadth-first search
 map<int, int> parallel_breadth_first_search( adjacency_list& graph, int src, map<int, int>& vert_orders) {
     map<int, int> node_level;
-    
-    omp_lock_t* l;
-    omp_lock_t* k;
-    
-    uint64 time_start, time_stop, runtime;
-    time_start = getTimeMs64();
-    
+    omp_lock_t l;
+    omp_lock_t k;
+
+    omp_init_lock(&l);
+    omp_init_lock(&k);
     // initialize visited and queue
     bool* visited = new bool[graph.size()];
     #pragma omp parallel for
     for(int i=0; i<graph.size(); i++)
-        visited[i]=false;
+    {
+          visited[i]=false;
+          //std::cout<<i<<std::endl;
+    }
     queue<int> next_verts;
-    
+
     /*
      parallel breadth-first search
      */
@@ -149,67 +154,67 @@ map<int, int> parallel_breadth_first_search( adjacency_list& graph, int src, map
     visited[src-1] = true;
     while(next_verts.size()>0)
     {
+
         vector<int> level;
         queue<int> temp_queue;
-        #pragma omp critical
-        while(next_verts.size()>0)
+        vector<int> temp_list;
+        while(next_verts.size() > 0)
         {
-            omp_set_lock(l);
-            int cur_node = next_verts.front();
-            next_verts.pop();
-            omp_unset_lock(l);
-            
-            node_level.insert(std::make_pair(cur_node, lev));	
+          temp_list.push_back(next_verts.front());
+          next_verts.pop();
+        }
+
+        #pragma omp parallel for
+        for(int i=0; i<temp_list.size(); i++)
+        {
+            //no need to lock this cuz its already divided to different threads by
+            // openmp
+            //std::cout<<omp_get_thread_num()<<std::endl;
+            int cur_node = temp_list[i];
+
+            node_level.insert(std::make_pair(cur_node, lev));
             vert_orders.insert(std::make_pair(cur_node, order++));
             list<int>nb;
             map<int, std::list<int> >::iterator it = graph.find(cur_node);
-            
+
             if(it != graph.end())
             {
-                omp_set_lock(l);
+                // 'it' is local for each thread
                 nb = it->second;
-                omp_unset_lock(l);
-                
+
                 std::list<int>::iterator it;
                 for (it = nb.begin(); it != nb.end(); ++it)
                 {
-                    omp_set_lock(l);
+                    omp_set_lock(&l);
                     if(visited[(*it)-1] == false)
                     {
-                        omp_set_lock(k);
-                        temp_queue.push(*it);
-                        omp_unset_lock(k);
-                        omp_set_lock(k);
+                        omp_set_lock(&k);
+                        next_verts.push(*it);
+                        omp_unset_lock(&k);
                         visited[(*it)-1] = true;
-                        omp_unset_lock(k);
                     }
-                    omp_unset_lock(l);
-                }	
+                    omp_unset_lock(&l);
+                }
             }
         }
-        #pragma omp barrier
-        next_verts = temp_queue;
+        //#pragma omp barrier
+        //next_verts = temp_queue;
         lev++;
-        
     }
-    
-    time_stop = getTimeMs64();
-    runtime = (time_stop - time_start) / 10;
-    cout << "The runtime is: " << runtime << " ms" << endl;
-    
+
     return node_level;
 }
 
 void generate_order_file(map<int, int> m) {
     ofstream ofs;
     ofs.open("pbfs_order.txt", ofstream::out);
-    
-    
+
+
     for(map<int, int>::iterator iter = m.begin(); iter != m.end(); ++iter)
         ofs << iter->first << " " << iter->second << "\n";
-    
+
     ofs.close();
-    
+
     return;
 }
 
@@ -230,23 +235,8 @@ uint64 getTimeMs64() {
     uint64 ret = tv.tv_usec;
     // convert micro secs (10^-6) to millisecs (10^-3)
     ret /= 1000;
-    
+
     // add seconds (10^0) after converting to millisecs (10^-3)
     ret += (tv.tv_sec*1000);
     return ret;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
